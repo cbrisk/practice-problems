2c60905c7f45d581ec010a2c6d5921da217df8e3
Chaim Brisk <cbrisk99@gmail.com>
2021-01-28 23:22:37 -0800

    completed knapsack

---
+++ knapsack.js
@@ -0,0 +1,12 @@
+function knapsackLight(value1, weight1, value2, weight2, maxW) {
+  if (weight1 + weight2 <= maxW) {
+    return value1 + value2;
+  } else if (weight1 <= maxW && weight2 <= maxW) {
+    return Math.max(value1, value2);
+  } else if (weight1 > maxW && weight2 > maxW) {
+    return 0;
+  } else if (weight1 <= maxW) {
+    return value1;
+  }
+  return value2;
+}
