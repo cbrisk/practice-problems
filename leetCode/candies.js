70ebf527463c3645316950a3dca5a3df2ee5ba03
Chaim Brisk <cbrisk99@gmail.com>
2021-02-23 17:50:02 -0800

    completed candies

---
+++ leetCode/candies.js
@@ -0,0 +1,12 @@
+var kidsWithCandies = function (candies, extraCandies) {
+  let max = Math.max(...candies);
+  let arr = [];
+  for (let i = 0; i < candies.length; i++) {
+    if (candies[i] + extraCandies >= max) {
+      arr.push(true);
+    } else {
+      arr.push(false);
+    }
+  }
+  return arr;
+};
