07a1bbdec6de7eafde8b098be74dd72c14aaa92f
Chaim Brisk <cbrisk99@gmail.com>
2021-03-06 23:02:05 -0800

    completed parking spaces

---
+++ leetCode/parkingSpaces.js
@@ -0,0 +1,12 @@
+var ParkingSystem = function (big, medium, small) {
+  this.spaces = [big, medium, small];
+};
+
+ParkingSystem.prototype.addCar = function (carType) {
+  if (this.spaces[carType - 1] >= 1) {
+    this.spaces[carType - 1]--;
+    return true;
+  } else {
+    return false;
+  }
+};
