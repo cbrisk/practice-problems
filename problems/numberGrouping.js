4ea9a4d0515b2b7962d81ff93548e3f409c2ed94
Chaim Brisk <cbrisk99@gmail.com>
2021-03-07 16:56:46 -0800

    completed number grouping

---
+++ problems/numberGrouping.js
@@ -0,0 +1,15 @@
+function numbersGrouping(a) {
+  let count = 0;
+  let obj = {}
+  a.sort((a, b) => a - b);
+  for (let i = 0; i < a.length; i++) {
+    if (obj[Math.ceil(a[i] / 10000)] === undefined) {
+      obj[Math.ceil(a[i] / 10000)] = 1;
+      count += 2
+    } else {
+      obj[Math.ceil(a[i] / 10000)]++;
+      count++;
+    }
+  }
+  return count;
+}
